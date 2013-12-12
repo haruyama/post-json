7678cdf2cd97547cc088541142de5965dc0f4278
HARUYAMA Seigo <haruyama@unixuser.org>
2013-12-12 15:32:33 +0900

    add Path, UserAgent, MobileCarrier and DateTime.

--- access_info.go
+++ access_info.go
@@ -3,15 +3,25 @@ package main
 import (
     "fmt"
     "math/rand"
+    "time"
 )
 
 const (
     MAX_USER_ID = 100000
+    MAX_PATH_ID = 50000
 )
 
+var userAgents = []string{"IE", "Moziila", "Chrome", "Opera", "Safari"}
+
+var mobileCarriers = []string{"", "Docomo", "AU", "Softbank", "E-mobile"}
+
 type AccessInfo struct {
-    Ip_address string `json:"ip_address"`
-    User_id    int64  `json:"user_id"`
+    IpAddress     string `json:"ip_address"`
+    User_id       int64  `json:"user_id"`
+    Path          string `json:"path"`
+    UserAgent     string `json:"user_agent"`
+    MobileCarrier string `json:"mobile_carrier"`
+    DateTime      string `json:"datetime"`
 }
 
 func userId() int64 {
@@ -23,6 +33,22 @@ func ipAddress() string {
     return fmt.Sprintf("%d.%d.%d.%d", num>>24, (num>>16)%256, (num>>8)%256, num%256)
 }
 
+func path() string {
+    return fmt.Sprintf("/path%d", rand.Intn(MAX_PATH_ID))
+}
+
+func userAgent() string {
+    return userAgents[rand.Intn(len(userAgents))]
+}
+
+func mobileCarrier() string {
+    return mobileCarriers[rand.Intn(len(mobileCarriers))]
+}
+
+func dateTime() string {
+    return time.Now().UTC().Format(time.RFC3339)
+}
+
 func GetAccessInfo() AccessInfo {
-    return AccessInfo{ipAddress(), userId()}
+    return AccessInfo{ipAddress(), userId(), path(), userAgent(), mobileCarrier(), dateTime()}
 }
